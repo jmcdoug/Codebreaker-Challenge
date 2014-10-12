8bfb0a220fe61e8764f6f6842c13d623e063210d
jmcdoug <jmcdoug@purdue.edu>
2014-10-11 21:10:28 -0400

    gets the password

---
+++ getPassword.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <string.h>
+
+char computePasswordCharacter(char current, char previous)
+{
+	return (current+previous-7) - 95*((current+previous-7)/95) + 32;
+}
+
+int main(int argc, char **argv)
+{
+	if(argc == 1)
+	{
+		printf("wtf man\n");
+		return 0;
+	}
+	
+	char * username = argv[1];
+	char password[strlen(username)+1];
+
+	char p = 59;
+	for(int i = 0; i < strlen(username); i++)
+	{
+		//printf("%c",computePasswordCharacter(username[i],p));
+
+		password[i] = computePasswordCharacter(username[i], p);
+		
+		p = computePasswordCharacter(username[i], p);
+
+	}
+
+	password[strlen(username)] = 0;
+	printf("%s\n",password);
+
+	char username2[strlen(password)];
+	p = 59; 
+	for(int i = 0; i < strlen(username); i++)
+	{
+			int fuck = password[i]-25-p;
+			
+			while(fuck < 33)
+			{
+				fuck += 95;
+			}
+			username2[i] = fuck;
+
+			p = password[i];
+	}
+	username2[strlen(password)] = 0;
+	printf("%s\n",username2);
+	
+	/*
+	for(int i = 0; i < 256; i++)
+	{
+		printf("%d\n", computePasswordCharacter(i,59));
+	}
+	*/
+
+	return 1;
+}
